6f78d7501b3db74ce7565bf21300662ffc3e8f56
Aryan Shinde <aryan.shinde20@vit.edu>
2023-12-09 23:34:29 +0530

    added server with middlewares

---
+++ server/db/conn.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+//Database URI from .env
+const MONGODB_URI = process.env.DATABASE3;
+
+// For connection with mongodb
+mongoose
+  .connect(MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Database connection successful');
+  })
+  .catch((err) => {
+    console.log('No connection with DB');
+    console.log(err);
+  });
